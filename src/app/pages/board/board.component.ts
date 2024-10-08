e762ed128ef8bdfe4ce695a93fead33990e99d18
John <jeobando11@hotmail.com>
2024-10-08 10:52:35 -0500

    page board

---
+++ src/app/pages/board/board.component.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import {
+  faBox,
+  faWaveSquare,
+  faClock,
+  faAngleUp,
+  faAngleDown,
+  faHeart,
+  faBorderAll,
+  faUsers,
+  faGear,
+} from '@fortawesome/free-solid-svg-icons';
+import { faTrello } from '@fortawesome/free-brands-svg-icons';
+
+@Component({
+  selector: 'app-board',
+  standalone: true,
+  imports: [
+    NavbarComponent,
+    OverlayModule,
+    FontAwesomeModule,
+    CdkAccordionModule,
+  ],
+  templateUrl: './board.component.html',
+})
+export class BoardComponent {
+  faTrello = faTrello;
+  faBox = faBox;
+  faWaveSquare = faWaveSquare;
+  faClock = faClock;
+  faAngleUp = faAngleUp;
+  faAngleDown = faAngleDown;
+  faHeart = faHeart;
+  faBorderAll = faBorderAll;
+  faUsers = faUsers;
+  faGear = faGear;
+
+  items = [
+    {
+      label: 'Item 1',
+      items: [
+        {
+          label: 'Sub Item 1.1',
+        },
+        {
+          label: 'Sub Item 1.2',
+        },
+      ],
+    },
+    {
+      label: 'Item 2',
+      items: [
+        {
+          label: 'Sub Item 2.1',
+        },
+      ],
+    },
+    {
+      label: 'Item 3',
+      items: [
+        {
+          label: 'Sub Item 3.1',
+        },
+        {
+          label: 'Sub Item 3.2',
+        },
+        {
+          label: 'Sub Item 3.3',
+        },
+      ],
+    },
+  ];
+}
--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,43 +1,98 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { OverlayModule } from '@angular/cdk/overlay';
-import { CdkAccordionModule } from '@angular/cdk/accordion';
-
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import {
-  faBox,
-  faWaveSquare,
-  faClock,
-  faAngleUp,
-  faAngleDown,
-  faHeart,
-  faBorderAll,
-  faUsers,
-  faGear,
-} from '@fortawesome/free-solid-svg-icons';
-import { faTrello } from '@fortawesome/free-brands-svg-icons';
+  CdkDragDrop,
+  DragDropModule,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { ToDo, Column } from '../../models/ToDo';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [
-    NavbarComponent,
-    OverlayModule,
-    FontAwesomeModule,
-    CdkAccordionModule,
-  ],
+  imports: [DragDropModule, NavbarComponent],
   templateUrl: './boards.component.html',
+  styles: [
+    `
+      /* Animate items as they're being sorted. */
+      .cdk-drop-list-dragging .cdk-drag {
+        transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
+      }
+
+      /* Animate an item that has been dropped. */
+      .cdk-drag-animating {
+        transition: transform 300ms cubic-bezier(0, 0, 0.2, 1);
+      }
+    `,
+  ],
 })
 export class BoardsComponent {
-  faTrello = faTrello;
-  faBox = faBox;
-  faWaveSquare = faWaveSquare;
-  faClock = faClock;
-  faAngleUp = faAngleUp;
-  faAngleDown = faAngleDown;
-  faHeart = faHeart;
-  faBorderAll = faBorderAll;
-  faUsers = faUsers;
-  faGear = faGear;
+  columns: Column[] = [
+    {
+      title: 'ToDo',
+      todos: [
+        {
+          id: '1',
+          title: 'Make dishes',
+        },
+        {
+          id: '2',
+          title: 'Buy a unicorn',
+        },
+      ],
+    },
+    {
+      title: 'Doing',
+      todos: [
+        {
+          id: '3',
+          title: 'Watch Angular Path in Platzi',
+        },
+      ],
+    },
+    {
+      title: 'Done',
+      todos: [
+        {
+          id: '4',
+          title: 'Play video games',
+        },
+      ],
+    },
+  ];
+
+  todos: ToDo[] = [];
+  doing: ToDo[] = [];
+  done: ToDo[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  drop(event: CdkDragDrop<ToDo[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+
+  addColumn() {
+    this.columns.push({
+      title: 'New Column',
+      todos: [],
+    });
+  }
 }
